17f26146e88da2ae23ee72149e9acfd50c8ebeed
hua86430 <k0981465867@gmail.com>
2021-07-06 22:47:35 +0800

    new

--- week5/index.js
+++ week5/index.js
@@ -1,135 +1,134 @@
 import productModal from "./productModal.js";
 
+VeeValidate.defineRule("email", VeeValidateRules["email"]);
+VeeValidate.defineRule("required", VeeValidateRules["required"]);
 
-VeeValidate.defineRule('email', VeeValidateRules['email']);
-VeeValidate.defineRule('required', VeeValidateRules['required']);
-
-VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+VeeValidateI18n.loadLocaleFromURL("./zh_TW.json");
 
 // Activate the locale
 VeeValidate.configure({
-  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  generateMessage: VeeValidateI18n.localize("zh_TW"),
   validateOnInput: true, // 調整為輸入字元立即進行驗證
 });
 
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
-      url: 'https://vue3-course-api.hexschool.io/',
+      url: "https://vue3-course-api.hexschool.io/",
       path: "hua430",
       products: [],
       product: {},
       loadingStatus: {
-        loadingItem: ""
+        loadingItem: "",
       },
       carts: {},
       order: {
-        "user": {
-          "name": "",
-          "email": "",
-          "tel": "",
-          "address": ""
+        user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: "",
         },
-        "message": ""
-      }
+        message: "",
+      },
     };
   },
   components: {
-    productModal
+    productModal,
   },
   methods: {
-    isPhone (value) {
+    isPhone(value) {
       const phoneNumber = /^(09)[0-9]{8}$/;
-      return phoneNumber.test(value) ? true : '需要正確的電話號碼';
+      return phoneNumber.test(value) ? true : "需要正確的電話號碼";
     },
-    getProducts () {
-      axios.get(`${this.url}api/${this.path}/products?`)
-        .then(res => {
-          this.products = res.data.products;
-        });
+    getProducts() {
+      axios.get(`${this.url}api/${this.path}/products?`).then((res) => {
+        this.products = res.data.products;
+      });
     },
-    getProduct (id) {
+    getProduct(id) {
       this.loadingStatus.loadingItem = id;
-      axios.get(`${this.url}api/${this.path}/product/${id}`)
-        .then(res => {
+      axios
+        .get(`${this.url}api/${this.path}/product/${id}`)
+        .then((res) => {
           this.loadingStatus.loadingItem = "";
           this.product = res.data.product;
           this.$refs.productModal.openModal();
-
         })
-        .catch(res => {
+        .catch((res) => {
           console.log(res.data);
         });
     },
-    addCart (id, qty = 1) {
+    addCart(id, qty = 1) {
       this.loadingStatus.loadingItem = id;
       const itemData = {
         product_id: id,
-        qty
+        qty,
       };
-      axios.post(`${this.url}api/${this.path}/cart`, {
-        data: itemData
-      })
-        .then(res => {
+      axios
+        .post(`${this.url}api/${this.path}/cart`, {
+          data: itemData,
+        })
+        .then((res) => {
           this.loadingStatus.loadingItem = "";
           this.$refs.productModal.hideModal();
           this.getCart();
           alert(res.data.message);
-
         })
-        .catch(res => {
+        .catch((res) => {
           console.log(res.data);
         });
     },
-    getCart () {
-      axios.get(`${this.url}api/${this.path}/cart`)
-        .then(res => {
+    getCart() {
+      axios
+        .get(`${this.url}api/${this.path}/cart`)
+        .then((res) => {
           this.carts = res.data.data;
         })
-        .catch(res => {
+        .catch((res) => {
           console.log(res.data);
         });
     },
-    updateCart (data) {
+    updateCart(data) {
       const itemData = {
         product_id: data.product_id,
         qty: data.qty,
       };
       this.loadingStatus.loadingItem = data.id;
 
-      axios.put(`${this.url}api/${this.path}/cart/${data.id}`, {
-        data: itemData
-      })
-        .then(res => {
+      axios
+        .put(`${this.url}api/${this.path}/cart/${data.id}`, {
+          data: itemData,
+        })
+        .then((res) => {
           this.loadingStatus.loadingItem = "";
           this.getCart();
           alert(res.data.message);
         })
-        .catch(res => {
+        .catch((res) => {
           console.log(res.data);
         });
     },
-    deleteItem (id) {
+    deleteItem(id) {
       this.loadingStatus.loadingItem = id;
-      axios.delete(`${this.url}api/${this.path}/cart/${id}`)
-        .then(res => {
-          this.loadingStatus.loadingItem = "";
-          this.getCart();
-          alert(res.data.message);
-        });
+      axios.delete(`${this.url}api/${this.path}/cart/${id}`).then((res) => {
+        this.loadingStatus.loadingItem = "";
+        this.getCart();
+        alert(res.data.message);
+      });
     },
-    deleteAll () {
-      axios.delete(`${this.url}api/${this.path}/carts`)
-        .then(res => {
-          this.getCart();
-          alert(res.data.message);
-        });
+    deleteAll() {
+      axios.delete(`${this.url}api/${this.path}/carts`).then((res) => {
+        this.getCart();
+        alert(res.data.message);
+      });
     },
-    onSubmit () {
-      axios.post(`${this.url}api/${this.path}/order`, {
-        data: this.order
-      })
-        .then(res => {
+    onSubmit() {
+      axios
+        .post(`${this.url}api/${this.path}/order`, {
+          data: this.order,
+        })
+        .then((res) => {
           if (res.data.success) {
             this.$refs.form.resetForm();
             this.getCart();
@@ -138,15 +137,15 @@ const app = Vue.createApp({
             alert(res.data.message);
           }
         });
-    }
+    },
   },
-  created () {
+  created() {
     this.getProducts();
     this.getCart();
-  }
+  },
 })
 
-  .component('VForm', VeeValidate.Form)
-  .component('VField', VeeValidate.Field)
-  .component('ErrorMessage', VeeValidate.ErrorMessage)
+  .component("VForm", VeeValidate.Form)
+  .component("VField", VeeValidate.Field)
+  .component("ErrorMessage", VeeValidate.ErrorMessage)
   .mount("#app");
---
+++ week7/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: "/week7/dist",
+};
